0f4fdd88073fe40d831cfab99ac0dad8d560504c
Zameen Cater <zcater40@stuy.edu>
2024-01-06 16:56:15 -0500

    Added functionality for arrow keys and some special keys

--- input.c
+++ input.c
@@ -1,7 +1,9 @@
 #include <unistd.h> // stdin_fileno
 #include <termios.h> // terminal functions
 #include <stdbool.h> // bool in_raw_mode
+#include <ctype.h> // isdigit
 #include "util.h"
+#include "input.h"
 
 struct editorinfo{
   struct termios orig_termios;
@@ -28,14 +30,41 @@ void clean_terminal(){
 }
 
 // Read one key from stdin
-char readKey(){
-  char c;
+// works for all except escape (waits for another character and treats it like a keycode)
+int readKey(){
+  char c, d, e, f;
   err(read(STDIN_FILENO, &c, 1), "reading keycode");
+
+  if(c=='\x1b'){ // c is 
+    read(STDIN_FILENO, &d, 1);
+    if(d=='['){ // input is [x
+      read(STDIN_FILENO, &e, 1);
+      if(e=='A') return ARROW_UP;
+      if(e=='B') return ARROW_DOWN;
+      if(e=='C') return ARROW_RIGHT;
+      if(e=='D') return ARROW_LEFT;
+      if(e=='H') return HOME;
+      if(e=='F') return END;
+      if('0'<=e && e<='9'){ // input is [x~
+        read(STDIN_FILENO, &f, 1);
+        if(f=='~'){
+          if(e=='2') return INSERT;
+          if(e=='3') return DELETE;
+          if(e=='5') return PAGE_UP;
+          if(e=='6') return PAGE_DOWN;
+        }
+      }
+    }
+  }
+
+
+
   return c;
 }
 
-void processKey(char c){
-  printf("%d: %c\r\n", c, c);
+void processKey(int c){
+  if(isprint(c)) printf("%d: %c\r\n", c, c);
+  else           printf("%d\r\n", c);
   if(c==(('q') & 0x1f)){ // quit on control-q
     exit(0);
   }
@@ -44,7 +73,7 @@ void processKey(char c){
 int main(){
   setup_terminal();
   atexit(clean_terminal);
-  char c;
+  int c;
 
   printf("^Q to quit\r\n");
   while(1){
