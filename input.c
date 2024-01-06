e6d7538fb33a9cd87192a5d643e2b4b88dab9ca2
Zameen Cater <zcater40@stuy.edu>
2024-01-06 18:08:28 -0500

    Rewrote input to use ncurses

--- input.c
+++ input.c
@@ -1,86 +1,50 @@
-#include <unistd.h> // stdin_fileno
-#include <termios.h> // terminal functions
-#include <stdbool.h> // bool in_raw_mode
-#include <ctype.h> // isdigit
-#include "util.h"
-#include "input.h"
+#include <stdlib.h> // atexit
+#include <ncurses.h> // ncurses
+#include <ctype.h> // isprint
 
-struct editorinfo{
-  struct termios orig_termios;
-  bool in_raw_mode;
-};
-struct editorinfo E;
+#define KEY_CTRL(c) ((c) & 0x1f)
 
-// Set terminal to raw mode
-void setup_terminal(){
-  struct termios term;
-  err(tcgetattr(STDIN_FILENO, &term), "setup tcgetattr"); 
-  E.orig_termios = term;
-
-  cfmakeraw(&term);
-  err(tcsetattr(STDIN_FILENO, TCSAFLUSH, &term), "setup tcsetattr");
-
-  E.in_raw_mode = true;
+void setup(){
+  initscr();
+  raw();
+  keypad(stdscr, TRUE);
+  noecho();
+  nonl();
+  curs_set(1);
 }
 
-// Revert terminal to the original interface
-void clean_terminal(){
-  err(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios), "cleaning tcsetattr");
-  E.in_raw_mode = false;
+void reset(){
+  endwin();
 }
 
-// Read one key from stdin
-// works for all except escape (waits for another character and treats it like a keycode)
-int readKey(){
-  char c, d, e, f;
-  err(read(STDIN_FILENO, &c, 1), "reading keycode");
-
-  if(c=='\x1b'){ // c is 
-    read(STDIN_FILENO, &d, 1);
-    if(d=='['){ // input is [x
-      read(STDIN_FILENO, &e, 1);
-      if(e=='A') return ARROW_UP;
-      if(e=='B') return ARROW_DOWN;
-      if(e=='C') return ARROW_RIGHT;
-      if(e=='D') return ARROW_LEFT;
-      if(e=='H') return HOME;
-      if(e=='F') return END;
-      if('0'<=e && e<='9'){ // input is [x~
-        read(STDIN_FILENO, &f, 1);
-        if(f=='~'){
-          if(e=='2') return INSERT;
-          if(e=='3') return DELETE;
-          if(e=='5') return PAGE_UP;
-          if(e=='6') return PAGE_DOWN;
-        }
-      }
-    }
-  }
-
-
-
-  return c;
-}
-
-void processKey(int c){
-  if(isprint(c)) printf("%d: %c\r\n", c, c);
-  else           printf("%d\r\n", c);
-  if(c==(('q') & 0x1f)){ // quit on control-q
-    exit(0);
-  }
+int getKey(){
+  int ch = getch();
+  return ch;
 }
 
 int main(){
-  setup_terminal();
-  atexit(clean_terminal);
-  int c;
 
-  printf("^Q to quit\r\n");
+  setup();
+  atexit(reset);
+
+  int ch;
+
   while(1){
-    c = readKey();
-    processKey(c);
+    ch = getKey();
+    if(ch==KEY_LEFT) printw("left");
+    else if(ch==KEY_RIGHT) printw("right");
+    else if(ch==KEY_UP) printw("up");
+    else if(ch==KEY_DOWN) printw("down");
+    else if(ch==KEY_ENTER) printw("enter");
+    else if(ch==KEY_CTRL('b')) printw("2");
+    else if(ch==KEY_CTRL('c')) printw("3");
+    else if(isprint(ch)) printw("%d: %c", ch, ch);
+    else printw("%d", ch);
+    printw("\n");
+    refresh();
+
+    if(ch==KEY_CTRL('q')) break;
   }
 
   return 0;
 }
-
